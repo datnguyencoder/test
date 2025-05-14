9a158ac32e6bb57b96bba6c16af581a81c34089b
datfullstack <datfullstack>
2025-05-14 21:56:34 +0700

    push spring

---
+++ src/test/java/dat/com/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package dat.com.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
